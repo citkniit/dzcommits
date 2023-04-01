ba95894ad16575ab2469a8778d11362b99e52dc9
citkniit <aleksandr200411y@mail.ru>
2023-04-01 12:48:12 +0300

    third commit

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 using namespace std;
 int fib(n){
-if (n == 1 || n == 2) return 1;
+if (n == 1) return 0;
+else if (n == 2) return 1;
 else return fib(n-1) + fib(n-2);
 } 
 int main(){
