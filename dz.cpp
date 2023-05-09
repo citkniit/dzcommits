68223bac8297a574e0e2ae977cac621cbd9f61bd
citkniit <aleksandr200411y@mail.ru>
2023-05-09 12:30:00 +0300

    Рекурcивный вывод чисел Фибоначи с помощью функции fib которая возвращает целое число. Ввод любого номера и получаем результат число под этим номером.Первое и второе число равны 1, каждое последующее n-ое это сумма числа с номером n-1 и числа с номером n-2

---
+++ dz.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int fib(n){
+if (n == 1 || n == 2) return 1;
+else return fib(n-1) + fin(n-2); 
+int main(){
+int n;
+cin >> n;
+cout << fib(n);
+return 0;
+}
+
